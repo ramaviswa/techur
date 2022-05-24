e6afe331ad6b2acca9dd085be2211304d5052aec
jagadish R <viswa4323@gmail.com>
2022-05-24 16:36:06 +0530

    Appgwpip

--- techurresources/apgw/applicationgateway.tf
+++ techurresources/apgw/applicationgateway.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "apgwpip" {
   name = var.apgwpip.pipname
-  resource_group_name = var.resourcegroup1.rganame
+  resource_group_name = var.resourcegroup1.rgname
   location = var.location
   allocation_method = "Dynamic"
 }
