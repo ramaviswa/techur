a66abc24664056a40b49d6b10a06d0a1733fb29e
jagadish R <viswa4323@gmail.com>
2022-05-31 15:47:22 +0530

    vmbastionpip

--- variables.tf
+++ variables.tf
@@ -33,5 +33,5 @@ variable "techsubnet4" {}
 variable "techaddress4" {}
 variable "techursubnet5" {}
 variable "techaddress5" {}
-variable "bastionpip3" {}
-variable "bastionpip4" {}
+variable "techurpip3" {}
+variable "techurpip4" {}
