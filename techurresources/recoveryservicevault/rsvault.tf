87d0120ab34f69f0b5901de811664fd6d5333e78
jagadish R <viswa4323@gmail.com>
2022-06-16 19:23:36 +0530

    rsv

--- techurresources/recoveryservicevault/rsvault.tf
+++ techurresources/recoveryservicevault/rsvault.tf
@@ -8,10 +8,10 @@ resource "azurerm_resource_group" "recoveryrg" {
   
 }
 
-resource "azurerm_recovery_service_vault" "recoverysv" {
+resource "azurerm_recovery_services_vault" "recoverysv" {
     name = var.recovryservicevult.rsvname
     location = var.location
-    resource_group_name = rsvrgoutput.name
+    resource_group_name = var.rsvault.name
     sku = "standard"
 
     soft_delete_enable = "true"
